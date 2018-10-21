5c22fc8aac4c77878d9d293d95b8ac4c583db1f7
Edison <edisonliu311@gmail.com>
2018-10-21 17:57:00 +0800

    完成keyPad模块、游戏设计模块、关于信息模块及bgm模块

--- app/src/main/java/com/example/edison/shudu/Music.java
+++ app/src/main/java/com/example/edison/shudu/Music.java
@@ -8,10 +8,18 @@ public class Music {
     private static MediaPlayer mp = null;
     public static void play(Context context, int resource){
         stop(context);
-
+        if(SettingActivity.getMusic(context)){
+            mp = MediaPlayer.create(context, resource);
+            mp.setLooping(true);
+            mp.start();
+        }
     }
 
     public static void stop(Context context){
-
+        if(mp != null){
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
     }
 }
